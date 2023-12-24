949a20ec969a5692e98bb395035e7698b3fbbdfa
KovachVolodymer <kovach.volodymyr1@student.uzhnu.edu.ua>
2023-12-24 21:07:59 +0200

    add cross origin anntotathion

--- src/main/java/com/spring/jwt/mongodb/controllers/subControllers/FilterFlightController.java
+++ src/main/java/com/spring/jwt/mongodb/controllers/subControllers/FilterFlightController.java
@@ -5,10 +5,7 @@ import com.spring.jwt.mongodb.repository.FlightsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -19,6 +16,7 @@ import java.util.Comparator;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/flights")
 public class FilterFlightController {
 
--- src/main/java/com/spring/jwt/mongodb/controllers/subControllers/FilterHotelController.java
+++ src/main/java/com/spring/jwt/mongodb/controllers/subControllers/FilterHotelController.java
@@ -4,10 +4,7 @@ import com.spring.jwt.mongodb.models.Hotel;
 import com.spring.jwt.mongodb.repository.HotelsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalTime;
 import java.util.Comparator;
@@ -16,6 +13,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/hotels")
 public class FilterHotelController {
 
